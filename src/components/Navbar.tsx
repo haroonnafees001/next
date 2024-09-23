e188867c8a3fc1f36ca38eb63dcf280a57fc8a7f
Haroon Nafees <haroon.nafees@gmail.com>
2024-09-23 21:36:45 +0500

    ui work

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../ui/navbar-menu";
+import { cn } from "@/lib/utils";
+
+
+export function navbar(){
+
+    return(
+        <div>
+            navbar
+        </div>
+    )
+}
