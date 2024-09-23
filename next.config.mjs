5c6ce52b75e3583f18fd913fc4699e8a80005882
Haroon Nafees <haroon.nafees@gmail.com>
2024-09-24 04:56:03 +0500

    web work

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'res.cloudinary.com']
+        domains: ['img.freepik.com', 'res.cloudinary.com']
     }
 };
 
